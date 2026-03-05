19b7585b6eef12cb0af6c94e118b206a2af8b5de
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-05 20:03:24 +0200

    qweqw

--- resources/views/content/dashboard/dashboards-analytics.blade.php
+++ resources/views/content/dashboard/dashboards-analytics.blade.php
@@ -109,6 +109,48 @@
     </div>
   </div>
 </div>
+<div class="row g-4 mb-4">
+  <div class="col-sm-6 col-xl-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex align-items-start justify-content-between">
+          <div class="content-left">
+            <span>@lang("Total Commission")</span>
+            <div class="d-flex align-items-center my-2">
+              <h3 class="mb-0 me-2">${{ number_format($totalCommission ?? 0, 2) }}</h3>
+            </div>
+            <p class="mb-0">@lang("From Transactions")</p>
+          </div>
+          <div class="avatar">
+            <span class="avatar-initial rounded bg-label-primary">
+              <i class="ti ti-currency-dollar ti-sm"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-xl-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex align-items-start justify-content-between">
+          <div class="content-left">
+            <span>@lang("Orders Commission")</span>
+            <div class="d-flex align-items-center my-2">
+              <h3 class="mb-0 me-2">${{ number_format($totalOrdersCommission ?? 0, 2) }}</h3>
+            </div>
+            <p class="mb-0">@lang("From Orders")</p>
+          </div>
+          <div class="avatar">
+            <span class="avatar-initial rounded bg-label-success">
+              <i class="ti ti-receipt ti-sm"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <div class="row">
   <!-- Earning Reports -->
   <div class="col-lg-6 mb-4">
